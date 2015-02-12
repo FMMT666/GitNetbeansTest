d9da9a8a317aac15db7eaee358b2eea43f9ab223
FMMT666 <fmmt666@gmx.tm>
2015-02-13 00:58:08 +0100

    not perfect, but seems to work...

--- src/main/java/org/askrwrk/asgittest/mainFrame.java
+++ src/main/java/org/askrwrk/asgittest/mainFrame.java
@@ -28,26 +28,44 @@ public class mainFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         butBurp = new javax.swing.JButton();
+        panelOne1 = new org.askrwrk.panels.panelOne();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         butBurp.setText("*burp*");
 
+        javax.swing.GroupLayout panelOne1Layout = new javax.swing.GroupLayout(panelOne1);
+        panelOne1.setLayout(panelOne1Layout);
+        panelOne1Layout.setHorizontalGroup(
+            panelOne1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        panelOne1Layout.setVerticalGroup(
+            panelOne1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 244, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(168, Short.MAX_VALUE)
-                .addComponent(butBurp)
-                .addGap(165, 165, 165))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(panelOne1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 313, Short.MAX_VALUE)
+                        .addComponent(butBurp)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(131, 131, 131)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(panelOne1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(butBurp)
-                .addContainerGap(146, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -90,5 +108,6 @@ public class mainFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton butBurp;
+    private org.askrwrk.panels.panelOne panelOne1;
     // End of variables declaration//GEN-END:variables
 }
